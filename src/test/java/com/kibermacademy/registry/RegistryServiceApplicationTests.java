9d454073239ba8470a8fb45b9c21d6acc7dde09e
Miguel Ramos <return.infox@gmail.com>
2023-08-10 19:25:31 -0400

    configuracion de eureka server para microservicios

---
+++ src/test/java/com/kibermacademy/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibermacademy.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
